d8a16e31650ce8596f4f509cb4c97c19148293ad
ninjasul@gmail.com <ninjasul@gmail.com>
2020-01-06 17:48:26 +0900

    History

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import About from "./About";
 import Profile from "./Profile";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
     return (
@@ -12,11 +13,13 @@ function App() {
                 <li><Link to="/">홈</Link></li>
                 <li><Link to="/about">소개</Link></li>
                 <li><Link to="/profiles">프로필 목록</Link></li>
+                <li><Link to="/history">히스토리 예제</Link></li>
             </ul>
             <hr />
             <Route path="/" component={Home} exact />
             <Route path="/about" component={About} />
             <Route path="/profiles" component={Profiles} />
+            <Route path="/history" component={HistorySample} />
         </div>
     );
 }
